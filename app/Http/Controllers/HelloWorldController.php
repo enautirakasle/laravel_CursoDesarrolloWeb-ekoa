f3d01c00a4b5d83fbd95dfd2012661b6495f2876
Eñaut AA <enaut.irakasle@gmail.com>
2024-10-30 11:32:56 +0100

    HelloWorldController martxan

---
+++ app/Http/Controllers/HelloWorldController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloWorldController extends Controller
+{
+    //
+    public function index(){
+        return view('hello-world', [
+            'text' => 'Kaixo!!!'
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+ // Start of Selection
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloWorldController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello-world', [HelloWorldController::class, 'index']);
