f1e26610056e0c569de9223372369b91423863ad
Eñaut AA <enaut.irakasle@gmail.com>
2024-11-30 21:33:23 +0100

    cruda egiten hasita

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,14 @@
 
  // Start of Selection
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HelloWorldController;
+use App\Models\Task;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/tasks', function(){
+    
+    $tasks = Task::take(20)->get();
+    dd($tasks);
 });
